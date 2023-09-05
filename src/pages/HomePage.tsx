155be284b4d734588371102f62cfa6acdc65aaa9
yashbhanu <bhanushaliyash2000@gmail.com>
2023-09-05 23:20:34 +0530

    home page improved & merged with footer'

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import Card from '../Components/Card'
+import Card from '../Components/Card/Card'
 
 interface CardProps {
   id: number;
@@ -44,7 +44,7 @@ const HomePage: React.FC = () => {
   return (
     <div className='w-full h-full min-h-screen home'>
       <section className='max-w-screen-2xl mx-auto'>
-        <div className='w-full mt-4 relative flex flex-row justify-center items-center'>
+        <section className='banner-search w-full mt-4 relative flex flex-row justify-center items-center'>
           <img className='w-full h-full m-auto' src="/assets/images/homePage.svg" alt='home' />
           <div className='text-6xl text-[#1F2937] font-bold absolute top-52'>Fresh Food that deserve to eat</div>
           <div className='absolute text-xl text-[#1F2937]'>Get your healthy foods & snacks delivered at your doorsteps all day everyday</div>
@@ -53,13 +53,15 @@ const HomePage: React.FC = () => {
             <input type='text' className='m-auto w-[100%] border-0 outline-none' placeholder='Search Vegetables and Fruits' />
             <button className='px-5 py-2 bg-[#4DBD7A] text-white rounded-lg'>Search</button>
           </div>
-        </div>
-        <section className='mt-14 grid grid-cols-3 grid-rows-auto justify-items-center'>
+        </section>
+
+        <section className='fresh-products mt-14 grid grid-cols-3 grid-rows-auto justify-items-center'>
           <img src='/assets/images/FruitCard1.svg' alt='fruit1' />
           <img src='/assets/images/FruitCard2.svg' alt='fruit1' />
           <img src='/assets/images/FruitCard3.svg' alt='fruit1' />
         </section>
-        <section className='px-10 mt-10'>
+
+        <section className='fruit-list px-10 mt-10'>
           <div className=''>
             <p className='text-2xl font-semibold my-4'>Fresh Fruits</p>
           </div>
@@ -73,7 +75,8 @@ const HomePage: React.FC = () => {
             }
           </div>
         </section>
-        <section className='px-10 mt-10'>
+
+        <section className='vegetable-list px-10 mt-10'>
           <div className=''>
             <p className='text-2xl font-semibold my-4'>Fresh Vegetables</p>
           </div>
@@ -87,11 +90,12 @@ const HomePage: React.FC = () => {
             }
           </div>
         </section>
-        <section className='px-10 mt-10'>
+
+        <section className='discount-banner px-10 mt-10'>
           <img src='/assets/images/vegBanner.svg' alt='vegBanner' />
         </section>
 
-        <section className='px-6'>
+        <section className='discount-list px-6'>
           <div className='mt-10 grid grid-cols-6 grid-rows-auto justify-items-center'>
             <img src='/assets/images/vcard1.svg' alt='vcard1' />
             <img src='/assets/images/vcard2.svg' alt='vcard2' />
@@ -108,6 +112,27 @@ const HomePage: React.FC = () => {
             <img src='/assets/images/vcard4.svg' alt='vcard4' />
             <img src='/assets/images/vcard4.svg' alt='vcard4' />
             <img src='/assets/images/vcard2.svg' alt='vcard2' />
+          </div>
+        </section>
+
+        <section className='how-works mt-20 px-10'>
+          <h1 className='text-4xl text-center font-bold text-[#1F2937]'>How It Works</h1>
+          <div className='grid grid-cols-3 gap-12 mt-16'>
+            <div className='flex justify-center items-center flex-col border-2 border-gray-300 rounded-xl p-10 gap-3'>
+              <img src='/assets/images/clip1.svg' alt='clip1' height={80} width={80} />
+              <p className='text-xl font-semibold'>Order Packed</p>
+              <p className='text-[#747875] text-center'>Your order will be packed safely and transferred to warehouse</p>
+            </div>
+            <div className='flex justify-center items-center flex-col border-2 border-gray-300 rounded-xl p-10 gap-3'>
+              <img src='/assets/images/clip2.svg' alt='clip1' height={80} width={80} />
+              <p className='text-xl font-semibold'> Over to the Carrier</p>
+              <p className='text-[#747875] text-center'>The order will be picked from the warehouse and out for delivery</p>
+            </div>
+            <div className='flex justify-center items-center flex-col border-2 border-gray-300 rounded-xl p-10 gap-3'>
+              <img src='/assets/images/clip3.svg' alt='clip1' height={80} width={80} />
+              <p className='text-xl font-semibold'>Out for delivery</p>
+              <p className='text-[#747875] text-center'>The order will be carefully delivered at your doorstep</p>
+            </div>
           </div>
         </section>
       </section>
