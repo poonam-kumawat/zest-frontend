3cf74a91ecfd6dafdd8eb377ae854618b2e5e90a
poonam-kumawat <mailpoonam2002@gmail.com>
2023-09-05 21:58:25 +0530

    footer add

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footerWrap  mt-10 py-10  bg-[#F9F9F9]">
+      <div className="md:grid md:grid-cols-3">
+        <div className="fisrtCol">
+          <h2 className="m-auto text-center text-[#4DBD7A] text-4xl font-bold">
+            Zest
+          </h2>
+          <p className="text-center m-auto text-[#747875] mt-10">
+            Copyright © 2023 Zest E-commerce
+          </p>
+          <p className="text-center m-auto text-[#747875] mt-10">
+            Order your daily & fresh veggies easily & directly from the farms
+          </p>
+        </div>
+        <div className="flex md:grid-cols-2">
+        <div className="secondCol md:mx-auto md:my-0 grid">
+          <div className="grid">
+            <a href="/">Home</a>
+            <a href="/">Delivery areas</a>
+            <a href="/">Carreir</a>
+            <a href="/">Press</a>
+          </div>
+        </div>
+        <div className="thirdCol md:mx-auto md:my-0 grid">
+          <div className="grid">
+            <a href="/">Privacy Policy</a>
+            <a href="/">Terms of use</a>
+            <a href="/">Blogs</a>
+            <a href="/">Aboutus</a>
+          </div>
+        </div>
+        </div>
+        <div className="forthCol grid text-center">
+          <div className="grid">
+            <div>
+              <p>Contact us</p>
+              <p>1800-313-3302</p>
+            </div>
+            <div>
+              <p>Follow us</p>
+              <div className="mt-3 px-24">
+                <div className="flex justify-between">
+                  <img
+                    className="h-8"
+                    src="/assets/icons/facebook.svg"
+                    alt="facebook"
+                  />
+                  <img
+                    className="h-8"
+                    src="/assets/icons/twitter.svg"
+                    alt="facebook"
+                  />
+                  <img
+                    className="h-8"
+                    src="/assets/icons/instagram.svg"
+                    alt="facebook"
+                  />
+                  <img
+                    className="h-8"
+                    src="/assets/icons/youtube.svg"
+                    alt="facebook"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
