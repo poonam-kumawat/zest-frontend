4c781c86b00461a70eda052599d367949f1ead94
yashbhanu <bhanushaliyash2000@gmail.com>
2023-10-22 23:39:14 +0530

    check if location valid or not

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,9 +1,13 @@
 import { globalaxiosInstance } from "./axiosSetup";
 
 export const getProducts = (filter: any) => {
-    return globalaxiosInstance.post(`/api/product`, filter);
-}
+  return globalaxiosInstance.post(`/api/product`, filter);
+};
 
-export const getCategories=()=>{
-    return globalaxiosInstance.get('/api/category/')
-}
+export const getCategories = () => {
+  return globalaxiosInstance.get("/api/category/");
+};
+
+export const getLocation = (filter: any) => {
+  return globalaxiosInstance.post(`/api/pincode/`, filter);
+};
