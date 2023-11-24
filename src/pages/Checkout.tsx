64321b54f2301d1cf5e0c8c1f1f74c21bbca14e2
yashbhanu <bhanushaliyash2000@gmail.com>
2023-11-25 00:19:01 +0530

    modal fixe

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { rootType } from "../Redux/rootReducer";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
-import AddressModal from "../Components/AddressModal/AddressModal";
 import {
   deleteAddress,
   generateOrder,
@@ -11,18 +8,18 @@ import {
   verifyPayment,
 } from "../services/api.service";
 import { LoaderHome } from "../Components/Common/Loader";
-import { protectedaxiosInstance } from "../services/axiosSetup";
 import { useNavigate } from "react-router-dom";
 import { emptyCart } from "../Redux/reducer/cartReducer";
 import { showErrorToast } from "../utils/helper";
 import { ToastContainer } from "react-toastify";
+import AddressView from "../Components/AddressView/AddressView";
 
 const Checkout = () => {
+  const dispatch = useDispatch();
+
   const [addresses, setAddreses] = useState<any>([]);
   const [loading, setLoading] = useState(true);
-  const [addressModal, setaddressModal] = useState(false);
   const [order, setorder] = useState<any>({});
-  const [editAddress, setEditAddress] = useState({});
 
   const { email } = useSelector((state: rootType) => state.user);
   const { totalAmount } = useSelector((state: rootType) => state.cart);
@@ -117,6 +114,7 @@ const Checkout = () => {
 
         if (resp.status === 200) {
           navigate("/");
+          dispatch(emptyCart());
         }
       },
       prefill: {
@@ -136,20 +134,6 @@ const Checkout = () => {
     paymentObject.open();
   }
 
-  const deleteUserAddress = async (id: string) => {
-    try {
-      setLoading(true);
-      const resp: any = await deleteAddress({ id });
-      if (resp.status === 200) {
-        const data = addresses.filter((item: any) => item._id !== id);
-        setAddreses(data);
-      }
-      setLoading(false);
-    } catch (error: any) {
-      showErrorToast(error.message);
-    }
-  };
-
   useEffect(() => {
     setLoading(true);
     getUserAddress();
@@ -166,80 +150,14 @@ const Checkout = () => {
           <div className="flex gap-6 md:flex-row flex-col">
             <div className="md:w-[60%] w-full flex flex-col">
               <div className="border border-[#D9DDDC] rounded-lg">
-                <div className="p-8 flex flex-col gap-3 w-full">
-                  <div className="flex justify-between gap-2">
-                    <div>
-                      <p className="text-[#656565] md:text-base lg:text-lg font-normal">
-                        Use a saved address or a new address
-                      </p>
-                    </div>
-                    <button
-                      type="submit"
-                      disabled={addresses.length >= 3}
-                      className={`${
-                        addresses.length >= 3 ? "bg-[#96e4b5]" : "bg-[#4DBD7A]"
-                      } px-2 py-2 h-10 xl:text-base lg:text-sm md:text-xs sm:text-sm text-xs font-medium text-white rounded-lg flex justify-center items-center gap-2`}
-                      onClick={() => {
-                        setEditAddress({});
-                        setaddressModal(!addressModal);
-                      }}
-                    >
-                      Add Address
-                      <img
-                        className="sm:block hidden"
-                        width={15}
-                        height={15}
-                        src="/assets/icons/plus-icon.svg"
-                        alt="add"
-                      />
-                    </button>
-                  </div>
-                  <div className="grid grid-cols-1 gap-4 w-full">
-                    {addresses.length > 0 &&
-                      addresses.map((addr: any) => (
-                        <div
-                          key={addr._id}
-                          onClick={() => createOrder(addr)}
-                          className={`${
-                            order.addressId === addr._id
-                              ? "border-2 border-[#4DBD7A]"
-                              : "border-dashed border-[#babdbc]"
-                          } cursor-pointer relative w-full border p-5 gap-3 rounded-lg flex flex-col`}
-                        >
-                          <FontAwesomeIcon
-                            className="absolute top-4 right-4"
-                            icon={faEdit}
-                            color="#4DBD7A"
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              setEditAddress(addr);
-                              setaddressModal(true);
-                            }}
-                          />
-                          <FontAwesomeIcon
-                            className="absolute top-4 right-12"
-                            icon={faTrash}
-                            color="#4DBD7A"
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              deleteUserAddress(addr._id);
-                            }}
-                          />
-                          <p className=" text-base font-medium">{addr.name}</p>
-                          <p className="text-sm text-[#656565]">
-                            {addr?.address.length > 300
-                              ? addr?.address.slice(0, 200) + "..."
-                              : addr?.address}
-                          </p>
-
-                          <p className="text-sm text-[#656565]">
-                            {" "}
-                            Contact Number : {addr.phoneNumber}
-                          </p>
-                        </div>
-                      ))}
-                  </div>
-                </div>
+                <AddressView
+                  payment={true}
+                  setLoading={setLoading}
+                  createOrder={createOrder}
+                  order={order}
+                  addresses={addresses}
+                  setAddreses={setAddreses}
+                />
               </div>
               <div className="bg-[#F4FBF7] p-3" />
               <div className="border border-[#D9DDDC] rounded-lg">
@@ -313,14 +231,6 @@ const Checkout = () => {
           </div>
         </div>
       </div>
-      {addressModal && (
-        <AddressModal
-          setaddressModal={setaddressModal}
-          addressData={addresses}
-          setAddreses={setAddreses}
-          editAddress={editAddress}
-        />
-      )}
       <ToastContainer />
     </div>
   ) : (
